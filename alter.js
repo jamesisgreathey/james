41186561879c7c9299ab27694657f06ed3a1f25c
jamesisgreathey <119701347+jamesisgreathey@users.noreply.github.com>
2023-02-15 18:47:30 +0000

    Delete alter.js

--- alter.js
+++
@@ -1,6 +0,0 @@
-function changeText()
-{
-    var items = ["monday","tuesday","wednesday","thursday","friday","saturday", "sunday"];
-    var day = new Date().getDay();
-    heading.textContent = items[day % items.length]
-}
